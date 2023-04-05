09c272e9f30051638377b696c116ad1d6758f4bf
David Dal Busco <david.dalbusco@outlook.com>
2023-04-05 07:34:47 +0200

    chore: import

--- src/lib/loader/worker.loader.ts
+++ src/lib/loader/worker.loader.ts
@@ -1,5 +1,7 @@
+// Does not work neither
+// import Worker from "$lib/workers/demo.worker?worker";
 
 export const initWorker = async () => {
   const Worker = await import("$lib/workers/demo.worker?worker");
-
+  console.log(Worker);
 };
